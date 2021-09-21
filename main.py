a17a5002256695fac6118c45f8a19edeb163ef37
paulflagel <paulflagel@gmail.com>
2021-09-21 16:45:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ nb_blocs = 4
 board = np.array([["-" for i in range(nb_blocs)] for j in range(3)])
 bloc_names = list(string.ascii_uppercase[:nb_blocs])
 input = random.sample(bloc_names, k=nb_blocs)
+output = bloc_names
 
 # initial state of the game
 board[0, :] = input
 
-print(board)
+print(board.T)
