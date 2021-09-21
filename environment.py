78a9d8ea5053ae305f1e5ef5e6937f56abbcae2c
= <=>
2021-09-21 16:48:51 +0200

    changed environment

--- environment.py
+++ environment.py
@@ -4,6 +4,8 @@ class Environment:
     def __init__(self, agents=None, positions=None, end_positions=None):
         self.agents = agents
         self.positions = positions #(i, j)
+        end_positions.append(None)
+        end_positions.insert(0, None)
         self.end_positions = end_positions
 
     def down_neighbor_id(self, name):
@@ -25,12 +27,23 @@ class Environment:
 
 
     def goal(self, name):
-        agent = self.agents[agent]
-        agent_under = down_neighbor(name)
-        agent_upper = upper_neighbor(name)
-        pass
+        agent_under = self.down_neighbor(name)
+        agent_upper = self.up_neighbor(name)
+        agent_index = None
+
+        for index, elt in enumerate(self.end_positions):
+            if elt == name:
+                agent_index = index
+                break
+        else:
+            raise Exception("The agent doesn't exist in the end positions")
+
+        return self.end_positions[agent_index-1] == agent_under and self.end_positions[agent_index+1] == agent_upper
+
+
 
     def move(self, name):
         pass
+
 
 
