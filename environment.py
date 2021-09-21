fc51ccb3ab7d7c1be84142809d2853d78413788e
paulflagel <paulflagel@gmail.com>
2021-09-21 23:17:07 +0200

    Formatted with black

--- environment.py
+++ environment.py
@@ -1,15 +1,15 @@
 import random as rd
 import numpy as np
 
-class Environment:
 
+class Environment:
     def __init__(self, agents=None, input_pattern=None, end_pattern=None):
         nb_blocs = len(agents)
-        
+
         self.agents = agents
         self.board = np.array([["-" for i in range(nb_blocs)] for j in range(3)])
-        
-        self.positions = {k:(0,v) for v, k in enumerate(input_pattern)}
+
+        self.positions = {k: (0, v) for v, k in enumerate(input_pattern)}
         self.board[0, :] = input_pattern
 
         # We add None at the beggining and at the end of the list of
@@ -22,7 +22,7 @@ class Environment:
         column, height = self.positions[name]
 
         for key, value in self.positions.items():
-            if value == (column, height-1):
+            if value == (column, height - 1):
                 return key
         return None
 
@@ -30,11 +30,9 @@ class Environment:
         column, height = self.positions[name]
 
         for key, value in self.positions.items():
-            if value == (column, height+1):
+            if value == (column, height + 1):
                 return key
         return None
-
-
 
     def goal(self, name):
         agent_down = self.down_neighbor_id(name)
@@ -48,9 +46,10 @@ class Environment:
         else:
             raise Exception("The agent doesn't exist in the end positions")
 
-        return self.end_pattern[agent_index-1] == agent_down and self.end_pattern[agent_index+1] == agent_up
-
-
+        return (
+            self.end_pattern[agent_index - 1] == agent_down
+            and self.end_pattern[agent_index + 1] == agent_up
+        )
 
     def move(self, name):
         columns = [0, 1, 2]
@@ -58,31 +57,23 @@ class Environment:
         columns.remove(column)
         new_column = rd.choice(columns)
 
-
         # Now, we will make sure that the block will go over the highest block
         max_height = -1
         for column, height in self.positions.values():
             if column == new_column:
                 if height >= max_height:
                     max_height = height
-        new_height = max_height+1
+        new_height = max_height + 1
         column, height = self.positions[name]
         self.positions[name] = (new_column, new_height)
         self.board[column, height] = "-"
         self.board[new_column, new_height] = name
-        
+
     def is_pushing(self, name):
         if name:
             return self.agents[name].pushing
         return False
-        
+
     def show_board(self):
         print(self.board)
-
-
-
-
-
-
-
 
--- main.py
+++ main.py
@@ -6,27 +6,28 @@ from environment import Environment
 from agent import Agent
 
 
-agents = {k: Agent(k) for k in list("ABCD")}
-input_pattern = list("ABCD")
-end_pattern = list("DCBA")
-env = Environment(agents, input_pattern, end_pattern)
+def main():
+    agents = {k: Agent(k) for k in list("ABCD")}
+    input_pattern = list("ABCD")
+    end_pattern = list("DCBA")
+    env = Environment(agents, input_pattern, end_pattern)
 
-end = False
-nb_rounds = 0
-while end == False:
-    
-    env.show_board()
-    print('\n')
+    end = False
+    nb_rounds = 0
 
-    if all([k.is_satisfied(env) for k in env.agents.values()]):
-        end = True
-    
-    for name, agent in env.agents.items():
-        agent.action(env)
-        
-    nb_rounds+=1
-print(f"Nombre de rounds pour te mettre KO : {nb_rounds}")
-    
-    
-    
+    while end == False:
+        env.show_board()
+        print("\n")
 
+        if all([k.is_satisfied(env) for k in env.agents.values()]):
+            end = True
+
+        for name, agent in env.agents.items():
+            agent.action(env)
+        nb_rounds += 1
+
+    print(f"Nombre de rounds pour te mettre KO : {nb_rounds}")
+
+
+if __name__ == "__main__":
+    main()
