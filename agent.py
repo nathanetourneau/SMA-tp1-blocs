3c2d7de323415652258907cd7835ed834017a458
paulflagel <paulflagel@gmail.com>
2021-09-21 16:26:05 +0200

    added agent methods

--- agent.py
+++ agent.py
@@ -1,10 +1,35 @@
 class Agent:
-    def __init_(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.goal = False
+        self.pushing = False
 
-    def move(self, x):
-        pass
+    def is_satisfied(self, environment):
+        if (environment.down_neighbor(self.name).pushing) or not (
+            environment.goal(self.name)
+        ):
+            return False
+        return True
+
+    def move(self, environment, column):
+        if self.is_free():
+            environment.move(self.name)
 
     def push(self):
+        self.pushing = True
+
+    def action(self, environment):
+        if not self.is_satisfied(environment):
+            if self.is_free():
+                self.move(environment)
+                self.pushing = False
+            else:
+                self.push(environment)
+
+            return True
+        else:
+            return False
+
+    def is_free(self, environment):
         pass
 
