1ed646ab49b62c4df53a018b63ebf431ca8469e0
= <=>
2021-09-21 16:26:33 +0200

    up

--- environment.py
+++ environment.py
@@ -4,30 +4,34 @@ class Environment:
     def __init__(self, agents=None, positions=None, end_positions=None):
         self.agents = agents
         self.positions = positions #(i, j)
+        self.end_positions = end_positions
 
-    def down_neighbor(self, name):
+    def down_neighbor_id(self, name):
         column, height = self.position[name]
 
         for key, value in self.positions.items():
             if value == (column, height-1):
-                return self.agents[key]
+                return key
         return None
 
-    def up_neighbor(self, name):
+    def up_neighbor_id(self, name):
         column, height = self.position[name]
 
         for key, value in self.positions.items():
             if value == (column, height+1):
-                return self.agents[key]
+                return key
         return None
+
+
 
     def goal(self, name):
         agent = self.agents[agent]
         agent_under = down_neighbor(name)
         agent_upper = upper_neighbor(name)
-        if
+        pass
 
     def move(self, name):
+        pass
 
 
 
